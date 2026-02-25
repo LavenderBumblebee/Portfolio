c16fbe3bfa690ce45a087735ad0e72642ef85601
Anthony Tran <142119636+LavenderBumblebee@users.noreply.github.com>
2026-02-24 22:30:39 -0800

    Update solution.sql
    Solution to SQL Murder Mystery Project

--- solution.sql
+++ solution.sql
@@ -1,27 +1,39 @@
+.headers on
+.mode column
 /* SQL Murder Mystery Solution
 Author: Anthony Tran
 Description: Investigating a murder in SQL City on Jan 15, 2018.
 */
 
--- STEP 1: Find the crime scene report
+-- STEP 1: Find the crime scene report of the murder, utilizing the city, type of crime, and date in the crime_scene_report table to filter the correct report 
+
 SELECT * FROM crime_scene_report 
 WHERE date = 20180115 AND type = 'murder' AND city = 'SQL City';
 
--- STEP 2: Find the two witnesses mentioned in the report
--- Witness 1: Lives at the last house on Northwestern Dr
-SELECT * FROM person 
+-- STEP 2: Identify the two witnesses mentioned in the report by referencing their personal information gathered from the crime scene report through querying the person's table. 
+-- Witness 1: Lives at the last house on Northwestern Dr (Filter for the highest address number on Northwestern Dr to identify the resident of the final house)
+
+SELECT id  FROM person 
 WHERE address_street_name = 'Northwestern Dr' 
 ORDER BY address_number DESC LIMIT 1;
 
--- Witness 2: Named Annabel, lives on Franklin Ave
-SELECT * FROM person 
+-- Witness 2: Named Annabel, lives on Franklin Ave (Used LIKE function in conjunction with address_street_name to single out the second witness)
+
+SELECT id FROM person 
 WHERE name LIKE 'Annabel%' AND address_street_name = 'Franklin Ave';
 
--- STEP 3: View the interview transcripts of the witnesses
-SELECT * FROM interview 
-WHERE person_id IN (14887, 16371);
+-- STEP 3: Retrieve interview transcripts using subqueries to dynamically pull person_id based on witness leads, ensuring the script remains functional if IDs in the person table change
 
--- STEP 4: The Final Join to find the killer (Jeremy Bowers)
+SELECT * FROM interview 
+WHERE person_id IN ((SELECT id  FROM person 
+WHERE address_street_name = 'Northwestern Dr' 
+ORDER BY address_number DESC LIMIT 1),
+(SELECT id FROM person 
+WHERE name LIKE 'Annabel%' AND address_street_name = 'Franklin Ave'));
+
+/* STEP 4: Join tables to cross reference relative Primary Keys and Foreign keys from the person, driver's license, get_fit_now_member, and get_fit_now_check_in tables to use all the evidence gathered to
+identify the culprit*/
+
 SELECT p.name, p.id, d.plate_number, f.membership_status
 FROM person p
 JOIN drivers_license d ON p.license_id = d.id
